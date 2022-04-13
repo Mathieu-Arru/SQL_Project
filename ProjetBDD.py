1abe4885f80ae5ee05fa99d2c1904c16042897f7
Mathieu <98914785+Mathieu-Arru@users.noreply.github.com>
2022-04-13 12:49:51 +0000

    commit dpt region

--- ProjetBDD.py
+++ ProjetBDD.py
@@ -1,42 +1,70 @@
 import numpy
-import psycopg2 # Python SQL driver for PostgreSQL
+import psycopg2  # Python SQL driver for PostgreSQL
 import psycopg2.extras
 import pandas as pd
 
 # Try to connect to an existing database
 print('Connexion a la base de donnees...')
-USERNAME="marru"
-PASSWORD="mdp" # `a remplacer par le mot de passe d’acces aux bases
+USERNAME = "marru"
+PASSWORD = "mdpbdd"  # `a remplacer par le mot de passe d’acces aux bases
 try:
-    conn = psycopg2.connect(host='localhost', dbname=USERNAME,user=USERNAME,password=PASSWORD)
-except Exception as e :
+    conn = psycopg2.connect(host='localhost',
+                            dbname=USERNAME,
+                            user=USERNAME,
+                            password=PASSWORD)
+except Exception as e:
     exit("Connexion impossible a la base de donnees: " + str(e))
-    
+
 print('Connecte a la base de donnees')
 # Preparation de l’execution des requetes (a ne faire qu’une fois)
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 # Création de la table Régions
-df_regions= pd.read_csv("region2020.csv",sep =",")
-cur.execute("""CREATE TABLE public.REGIONS(
-reg int PRIMARY KEY NOT NULL,
-cheflieu char(5) NOT NULL,
-tncc int NOT NULL,
-ncc varchar(30) NOT NULL,
-nccenr varchar(30) NOT NULL,
+df_regions = pd.read_csv("region2020.csv", sep=",")
+cur.execute("""CREATE TABLE public.region(
+IdR int PRIMARY KEY NOT NULL,
+nom varchar(30) NOT NULL,
 libelle varchar(30) NOT NULL; """)
-print("Table REGIONS creee avec succes dans PostgreSQL")
+print("Table region creee avec succes dans PostgreSQL")
 
-# Création de la table Départements 
-df_departements= pd.read_csv("departement2020.csv",sep =",")
-cur.execute("""CREATE TABLE public.DEPARTEMENTS(
-dep int PRIMARY KEY NOT NULL,
-reg int NOT NULL references REGIONS(reg),
-cheflieu char(5) NOT NULL,
-tncc int NOT NULL,
-ncc varchar(30) NOT NULL,
-nccenr varchar(30) NOT NULL,
+# Création de la table Départements
+df_departements = pd.read_csv("departement2020.csv", sep=",")
+cur.execute("""CREATE TABLE public.departement(
+IdD int PRIMARY KEY NOT NULL,
+IdR int NOT NULL references region(IdR),
+nom varchar(30) NOT NULL,
 libelle varchar(30) NOT NULL; """)
-print("Table DEPARTEMENTS creee avec succes dans PostgreSQL")
+print("Table departement creee avec succes dans PostgreSQL")
 
 
+# Fonction d'insertion dans les tables
+def insert(dataframe, command, nom=""):
+    print("Execution sur la base de donnees de la commande d’insertion avec les valeurs")
+    for i in range(dataframe.shape[0]):
+        values = []
+        for j in range(len(dataframe.columns)):
+            if type(dataframe.iloc[i][j]) is numpy.int64:
+                values.append(int(dataframe.iloc[i][j]))
+            else:
+                values.append(dataframe.iloc[i][j])
+        try:
+            # Lancement de la requête.
+            cur.execute(command, values)
+        except Exception as e:
+            # en cas d’erreur, fermeture de la connexion
+            cur.close()
+            conn.close()
+            exit("error when running: " + command + " : " + str(e))
+    # Nombre de lignes inserees
+    count = cur.rowcount
+    print(count,
+          f"enregistrement(s) insere(s) avec succes dans la table {nom}.")
+
+  
+# Insertion de la table Régions
+command = "INSERT INTO region(IdR,nom,libelle) VALUES (%s,%s,%s)"
+insert(df_regions, command, "region")
+
+# Insertion de la table Départements
+command = "INSERT INTO departement(IdD,IdR,nom,libelle) VALUES (%s,%s,%s,%s)"
+insert(df_departements, command, "departement")
---
+++ parser.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+df = pd.ExcelFile("Evolution_population_2012-2020.xlsx")
+print(df)
